284d845b73c2d9e793f6bb7d7584b887704ed5ff
letJamie <stillbut@naver.com>
2020-06-14 00:01:57 +0900

    Add sound file and function

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,15 +6,30 @@
 //  Copyright © 2020 Jamie. All rights reserved.
 //
 
+
+
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        //var name = self.title
+        //playSound(name)
+        playSound(name: sender.currentTitle!)
+    }
+    
+    func playSound(name: String) {
+        let url = Bundle.main.url(forResource: name, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
 
+    }
 }
-
